d63b2fa4ad7207cb63e6d68bb39f773bf61ac635
Venkat Swaraj <sathiraju829@gmail.com>
2023-01-28 19:46:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #Write your code here.
+number = eval(input())
+number = [str(x) for x in number]
+for i in range(len(number)):
+    temp = ""
+    while number[i]:
+        if len(number[i])<3:
+            temp = number[i][-1:-4:-1] + temp
+        else:
+            temp = number[i][-1:-4:-1] + "," +temp
+        number[i] = number[i][:-4]
+        
+print(number)
